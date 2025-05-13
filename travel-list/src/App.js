3a1cc5ded13ef9d483e76a9acdb67d9d59d66a73
Rachit Jain <rachitjain9596@gmail.com>
2025-05-13 10:30:00 +0530

    build layout

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,37 @@
 export default function App() {
-	return <div>Hello</div>;
+	return (
+		<div className="app">
+			<Logo />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	);
+}
+
+function Logo() {
+	return <h1>Far Away</h1>;
+}
+
+function Form() {
+	return (
+		<div className="add-form">
+			<h3>What do you need for the trip?</h3>
+		</div>
+	);
+}
+
+function PackingList() {
+	return <div className="list">LIST</div>;
+}
+
+function Stats() {
+	return (
+		<footer className="footer">
+			<em>
+				You have X items on your list, & you have already packed x%
+				items.
+			</em>
+		</footer>
+	);
 }
