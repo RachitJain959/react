b70da582c35af7369315733184480b820d16c169
Rachit Jain <rachitjain9596@gmail.com>
2025-06-12 10:30:00 +0530

    new friend1

--- eat-split/src/App.js
+++ eat-split/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 const initialFriends = [
 	{
@@ -49,7 +49,7 @@ function FriendsList() {
 	return (
 		<ul>
 			{friends.map((friend) => (
-				<Friend friend={friend} />
+				<Friend friend={friend} key={friend.id} />
 			))}
 		</ul>
 	);
@@ -85,13 +85,37 @@ function Button({ children, onClick }) {
 }
 
 function FormAddFriend() {
+	const [name, setName] = useState("");
+	const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const newFriend = {
+			name,
+			image,
+			id: crypto.randomUUID(),
+			balance: 0,
+		};
+
+		console.log(newFriend);
+	}
+
 	return (
-		<form className="form-add-friend">
+		<form className="form-add-friend" onSubmit={handleSubmit}>
 			<label>Friend Name</label>
-			<input type="text" />
+			<input
+				type="text"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
 
 			<label>Image URL</label>
-			<input type="text" />
+			<input
+				type="text"
+				value={image}
+				onChange={(e) => setImage(e.target.value)}
+			/>
 
 			<Button>Add</Button>
 		</form>
