cc1241eb50d09e69dc5f4b9294cac685fd997b1b
Rachit Jain <rachitjain9596@gmail.com>
2025-06-24 10:30:00 +0530

    Search

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -43,23 +43,30 @@ const tempWatchedData = [
 ];
 
 function NavBar() {
-	const [query, setQuery] = useState("");
 	const [movies, setMovies] = useState(tempMovieData);
 
 	return (
 		<nav className="nav-bar">
 			<Logo />
-			<input
-				className="search"
-				type="text"
-				placeholder="Search movies..."
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-			/>
+			<Search />
 			<p className="num-results">
 				Found <strong>{movies.length}</strong> results
 			</p>
 		</nav>
+	);
+}
+
+function Search() {
+	const [query, setQuery] = useState("");
+
+	return (
+		<input
+			className="search"
+			type="text"
+			placeholder="Search movies..."
+			value={query}
+			onChange={(e) => setQuery(e.target.value)}
+		/>
 	);
 }
 
