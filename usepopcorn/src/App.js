0d3093859d7eb98c98afd5d0bb1922526f9d34f5
Rachit Jain <rachitjain9596@gmail.com>
2025-08-08 10:30:00 +0530

    document title

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -322,6 +322,14 @@ function MovieDetails({ selectedId, setSelectedId, onAddWatched, watched }) {
 		[selectedId],
 	);
 
+	useEffect(
+		function () {
+			if (!title) return;
+			document.title = `Movie | ${title}`;
+		},
+		[title],
+	);
+
 	return (
 		<div className="details">
 			{isLoading ? (
