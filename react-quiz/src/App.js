b72a59461219ef7bb48af7f294045797322d37b7
Rachit Jain <rachitjain9596@gmail.com>
2025-09-03 10:30:00 +0530

    display progress

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -2,10 +2,11 @@ import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
-import Error from "./Error";
+import ErrorPage from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
 	questions: [],
@@ -16,6 +17,7 @@ const initialState = {
 	answer: null,
 	points: 0,
 };
+
 function reducer(state, action) {
 	switch (action.type) {
 		case "dataReceived":
@@ -47,11 +49,16 @@ function reducer(state, action) {
 }
 
 export default function App() {
-	const [{ questions, status, index, answer }, dispatch] = useReducer(
+	const [{ questions, status, index, answer, points }, dispatch] = useReducer(
 		reducer,
 		initialState,
 	);
 
+	const numQuestions = questions.length;
+	const maxPossiblePoints = questions.reduce(
+		(prev, cur) => prev + cur.points,
+		0,
+	);
 	useEffect(function () {
 		fetch("http://localhost:8000/questions")
 			.then((res) => res.json())
@@ -64,7 +71,7 @@ export default function App() {
 			<Header />
 			<Main>
 				{status === "loading" && <Loader />}
-				{status === "error" && <Error />}
+				{status === "error" && <ErrorPage />}
 				{status === "ready" && (
 					<StartScreen
 						numQuestions={questions.length}
@@ -73,6 +80,13 @@ export default function App() {
 				)}
 				{status === "active" && (
 					<>
+						<Progress
+							index={index}
+							numQuestions={numQuestions}
+							points={points}
+							maxPossiblePoints={maxPossiblePoints}
+							answer={answer}
+						/>
 						<Question
 							question={questions[index]}
 							dispatch={dispatch}
