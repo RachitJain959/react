2e15acefdcce7877552ed1e0fe6c470b595d1a57
Rachit Jain <rachitjain9596@gmail.com>
2025-09-06 10:30:00 +0530

    highscore

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -17,6 +17,7 @@ const initialState = {
 	index: 0,
 	answer: null,
 	points: 0,
+	highscore: 0,
 };
 
 function reducer(state, action) {
@@ -45,7 +46,14 @@ function reducer(state, action) {
 			return { ...state, index: state.index + 1, answer: null };
 
 		case "finish":
-			return { ...state, status: "finished" };
+			return {
+				...state,
+				status: "finished",
+				highscore:
+					state.points > state.highscore
+						? state.points
+						: state.highscore,
+			};
 
 		default:
 			throw new Error("Unknown action");
@@ -53,10 +61,8 @@ function reducer(state, action) {
 }
 
 export default function App() {
-	const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-		reducer,
-		initialState,
-	);
+	const [{ questions, status, index, answer, points, highscore }, dispatch] =
+		useReducer(reducer, initialState);
 
 	const numQuestions = questions.length;
 	const maxPossiblePoints = questions.reduce(
@@ -109,6 +115,7 @@ export default function App() {
 					<FinishScreen
 						points={points}
 						maxPossiblePoints={maxPossiblePoints}
+						highscore={highscore}
 					/>
 				)}
 			</Main>
--- react-quiz/src/FinishScreen.js
+++ react-quiz/src/FinishScreen.js
@@ -1,11 +1,14 @@
-function FinishScreen({ points, maxPossiblePoints }) {
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
 	const percentage = (points / maxPossiblePoints) * 100;
 
 	return (
-		<p className="result">
-			You scored {points} out of {maxPossiblePoints} (
-			{Math.ceil(percentage)}%)
-		</p>
+		<>
+			<p className="result">
+				You scored {points} out of {maxPossiblePoints} (
+				{Math.ceil(percentage)}%)
+			</p>
+			<p className="highscore">(Highscore: {highscore}) </p>
+		</>
 	);
 }
 
