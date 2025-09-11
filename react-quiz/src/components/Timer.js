24780aa9aad2ee1bb717bd50e75a4210d6dd98f9
Rachit Jain <rachitjain9596@gmail.com>
2025-09-11 10:30:00 +0530

    fin

--- react-quiz/src/components/Timer.js
+++ react-quiz/src/components/Timer.js
@@ -1,16 +1,28 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+	const mins = Math.floor(secondsRemaining / 60);
+	const secs = secondsRemaining % 60;
+
 	useEffect(
 		function () {
-			setInterval(function () {
+			const id = setInterval(function () {
 				dispatch({ type: "timer" });
 			}, 1000);
+
+			// if not for cleanup function, multiple timers will keep running simultaneously from previous iterations
+			return () => clearInterval(id);
 		},
 		[dispatch],
 	);
 
-	return <div className="timer">{secondsRemaining}</div>;
+	return (
+		<div className="timer">
+			{mins < 10 && "0"}
+			{mins}:{secs < 10 && "0"}
+			{secs}
+		</div>
+	);
 }
 
 export default Timer;
