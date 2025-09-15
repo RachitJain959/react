179999e138d6a43ab867f20d1093ecf10e48a869
Rachit Jain <rachitjain9596@gmail.com>
2025-09-15 10:30:00 +0530

    App.jsx

---
+++ world-wide/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="login" element={<Login />} />
+				<Route path="app" element={<AppLayout />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
