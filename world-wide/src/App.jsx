a70fb49234be8dfbcd9e721d3eea2912f84046c2
Rachit Jain <rachitjain9596@gmail.com>
2025-10-08 10:30:00 +0530

    implement city list 1

--- world-wide/src/App.jsx
+++ world-wide/src/App.jsx
@@ -6,8 +6,31 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
 function App() {
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(function () {
+		async function fetchCities() {
+			try {
+				setIsLoading(true);
+
+				const res = await fetch(
+					`${import.meta.env.VITE_BASE_URL}/cities`,
+				);
+				const data = await res.json();
+				setCities(data);
+			} catch (err) {
+				alert(err);
+			} finally {
+				setIsLoading(false);
+			}
+		}
+		fetchCities();
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
