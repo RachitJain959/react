df42d50b96ad2ea601330702fd0079862ad9acf3
Rachit Jain <rachitjain9596@gmail.com>
2025-10-11 10:30:00 +0530

    implement city list 3-Message

--- world-wide/src/components/CityList.jsx
+++ world-wide/src/components/CityList.jsx
@@ -1,7 +1,13 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
+import Message from "./Message";
+import Spinner from "./Spinner";
 
 function CityList({ cities, isLoading }) {
+	if (isLoading) return <Spinner />;
+
+	if (!cities.length) return <Message message="Add your city to the list" />;
+
 	return (
 		<ul className={styles.cityList}>
 			{cities.map((city) => (
