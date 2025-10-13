80a579cee5b2c742860d8de0196c073d66ce3369
Rachit Jain <rachitjain9596@gmail.com>
2025-10-13 10:30:00 +0530

    implement country list fin

--- world-wide/src/components/CountryList.jsx
+++ world-wide/src/components/CountryList.jsx
@@ -1,4 +1,4 @@
-import CityItem from "./CityItem";
+import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
@@ -8,10 +8,17 @@ function CountryList({ cities, isLoading }) {
 
 	if (!cities.length) return <Message message="Add your city to the list" />;
 
+	// if arr does not contain the current iterating city's country, then add the country of that city
+	const countries = cities.reduce((arr, city) => {
+		if (!arr.map((el) => el.country).includes(city.country))
+			return [...arr, { country: city.country, emoji: city.emoji }];
+		else return arr;
+	}, []);
+
 	return (
 		<ul className={styles.countryList}>
-			{cities.map((city) => (
-				<CityItem city={city} key={city.id} />
+			{countries.map((country) => (
+				<CountryItem country={country} key={country} />
 			))}
 		</ul>
 	);
