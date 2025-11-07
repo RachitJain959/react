7d41ab75af16f01b7979f703b63a446f643b1ebc
Rachit Jain <rachitjain9596@gmail.com>
2025-11-07 10:30:00 +0530

    worldwide-include map leaflet@4

--- world-wide/src/components/Map.jsx
+++ world-wide/src/components/Map.jsx
@@ -1,7 +1,11 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
 function Map() {
 	const [searchParams, setSearchParams] = useSearchParams();
+
+	const [mapPosition, setMapPosition] = useState([40, 0]);
 
 	const navigate = useNavigate();
 
@@ -9,8 +13,23 @@ function Map() {
 	const lng = searchParams.get("lng");
 
 	return (
-		<div className={styles.mapContainer} onClick={() => navigate("form")}>
-			lat={lat} lng={lng}
+		<div className={styles.mapContainer}>
+			<MapContainer
+				center={mapPosition}
+				zoom={13}
+				scrollWheelZoom={true}
+				className={styles.map}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+				/>
+				<Marker position={mapPosition}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
 		</div>
 	);
 }
