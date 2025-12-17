a57e0daa44d4466a03f23e620c3829acce5cc7e4
Rachit Jain <rachitjain9596@gmail.com>
2025-12-17 10:30:00 +0530

    redux-intro redux i

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
 	balance: 0,
 	loan: 0,
@@ -24,3 +26,9 @@ function reducer(state = initialState, action) {
 			return state;
 	}
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+
+console.log(store.getState());
