f7e6261819fb47e5f12b2972371ded93e9213354
Rachit Jain <rachitjain9596@gmail.com>
2025-12-23 10:30:00 +0530

    redux-intro customerSlice

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,47 +1,12 @@
 import { combineReducers, createStore } from "redux";
 
-const initialStateCustomer = {
-	fullName: "",
-	nationalID: "",
-	createdAt: "",
-};
-
-function customerReducer(state = initialStateCustomer, action) {
-	switch (action.type) {
-		case "customer/createCustomer":
-			return {
-				...state,
-				fullName: action.payload.fullName,
-				nationalID: action.payload.nationalID,
-				createdAt: action.payload.createdAt,
-			};
-		case "customer/updateCustomer":
-			return { ...state, fullName: action.payload.fullName };
-		default:
-			return state;
-	}
-}
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
 const rootReducer = combineReducers({
 	account: accountReducer,
 	customer: customerReducer,
 });
 const store = createStore(rootReducer);
-
-store.dispatch(deposit(500));
-store.dispatch(withdraw(500));
-store.dispatch(requestLoan(5000, "Buy car"));
-store.dispatch(payLoan());
-
-function createCustomer(fullName, nationalID) {
-	return {
-		type: "customer/createCustomer",
-		payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-	};
-}
-
-function updateCustomer(fullName) {
-	return { type: "customer/updateCustomer", payload: fullName };
-}
 
 console.log(store.getState());
