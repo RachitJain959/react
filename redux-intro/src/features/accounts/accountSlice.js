c51af21486389d3baa2c0c1327785055e1342809
Rachit Jain <rachitjain9596@gmail.com>
2026-01-05 10:30:00 +0530

    redux-intro accountSlice p1- reducers

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -1,61 +1,90 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialStateAccount = {
+const initialState = {
 	balance: 0,
 	loan: 0,
 	loanPurpose: "",
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
-	switch (action.type) {
-		case "account/deposit":
-			return { ...state, balance: state.balance + action.payload };
-		case "account/withdraw":
-			return { ...state, balance: state.balance - action.payload };
-		case "account/requestLoan":
+const accountSlice = createSlice({
+	name: "account",
+	initialState,
+	reducers: {
+		deposit(state, action) {
+			state.balance = state.blance + action.payload;
+		},
+
+		withdraw(state, action) {
+			state.balance = state.blance - action.payload;
+		},
+
+		requestLoan(state, action) {
 			if (state.loan > 0) return;
-			return {
-				...state,
-				loan: action.payload.amount,
-				loanPurpose: action.payload.loanPurpose,
-				balance: state.balance + action.payload.amount,
-			};
-		case "account/payLoan":
-			return {
-				...state,
-				loan: 0,
-				balance: state.balance - state.loan,
-				loanPurpose: "",
-			};
-		default:
-			return state;
-	}
-}
+			state.loan = action.payload.amount;
+			state.loanPurpose = action.payload.loanPurpose;
+			state.balance = state.balance + action.payload.amount;
+		},
 
-export function deposit(amount, currency) {
-	// this will return an object which tells redux to dispatch immediately
-	if (currency === "USD") return { type: "account/deposit", payload: amount };
+		payLoan(state, action) {
+			state.loan = 0;
+			state.loanPurpose = "";
+			state.balance = state.balance - state.loan;
+		},
+	},
+});
 
-	// if a function is returned to dispatch, redux knows it has a side effect(api call) so this will be redirected to middleware. Dispatch is not executed immediately
-	return async function (dispatch, getState) {
-		const res = await fetch(
-			`https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${currency}&to=USD`,
-		);
+console.log(accountSlice);
 
-		const data = await res.json();
-		const converted = data.rates.USD;
-		dispatch({ type: "account/deposit", payload: converted });
-	};
-}
+// export default function accountReducer(state = initialState, action) {
+// 	switch (action.type) {
+// 		case "account/deposit":
+// 			return { ...state, balance: state.balance + action.payload };
+// 		case "account/withdraw":
+// 			return { ...state, balance: state.balance - action.payload };
+// 		case "account/requestLoan":
+// 			if (state.loan > 0) return;
+// 			return {
+// 				...state,
+// 				loan: action.payload.amount,
+// 				loanPurpose: action.payload.loanPurpose,
+// 				balance: state.balance + action.payload.amount,
+// 			};
+// 		case "account/payLoan":
+// 			return {
+// 				...state,
+// 				loan: 0,
+// 				balance: state.balance - state.loan,
+// 				loanPurpose: "",
+// 			};
+// 		default:
+// 			return state;
+// 	}
+// }
 
-export function withdraw(amount) {
-	return { type: "account/withdraw", payload: amount };
-}
+// export function deposit(amount, currency) {
+// 	// this will return an object which tells redux to dispatch immediately
+// 	if (currency === "USD") return { type: "account/deposit", payload: amount };
 
-export function requestLoan(amount, loanPurpose) {
-	return { type: "account/requestLoan", payload: { amount, loanPurpose } };
-}
+// 	// if a function is returned to dispatch, redux knows it has a side effect(api call) so this will be redirected to middleware. Dispatch is not executed immediately
+// 	return async function (dispatch, getState) {
+// 		const res = await fetch(
+// 			`https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${currency}&to=USD`,
+// 		);
 
-export function payLoan() {
-	return { type: "account/payLoan" };
-}
+// 		const data = await res.json();
+// 		const converted = data.rates.USD;
+// 		dispatch({ type: "account/deposit", payload: converted });
+// 	};
+// }
+
+// export function withdraw(amount) {
+// 	return { type: "account/withdraw", payload: amount };
+// }
+
+// export function requestLoan(amount, loanPurpose) {
+// 	return { type: "account/requestLoan", payload: { amount, loanPurpose } };
+// }
+
+// export function payLoan() {
+// 	return { type: "account/payLoan" };
+// }
