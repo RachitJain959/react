a0cee99d6f52b578c941ca34c5e100871a022986
Rachit Jain <rachitjain9596@gmail.com>
2026-02-11 10:30:00 +0530

    fast-react-pizza isSubmitting createOrder

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -33,6 +33,9 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+	const navigation = useNavigation();
+	const isSubmitting = navigation.state === "submitting";
+
 	// const [withPriority, setWithPriority] = useState(false);
 	const cart = fakeCart;
 
@@ -79,7 +82,9 @@ function CreateOrder() {
 						name="cart"
 						value={JSON.stringify(cart)}
 					/>
-					<button>Order now</button>
+					<button disabled={isSubmitting}>
+						{isSubmitting ? "Placing Order..." : "Order now"}
+					</button>
 				</div>
 			</Form>
 		</div>
