fbb3a0020c22ea1af2ffd052b15decaabfba2350
Rachit Jain <rachitjain9596@gmail.com>
2026-02-17 10:30:00 +0530

    fast-react-pizza tailwind flexbox

--- fast-react-pizza/src/features/cart/CartOverview.jsx
+++ fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 px-4 py-4 text-stone-200 uppercase text-sm md:text-base sm:px-6">
+    <div className="bg-stone-800 px-4 py-4 text-stone-200 uppercase text-sm md:text-base sm:px-6 items-center justify-between flex">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
--- fast-react-pizza/src/features/users/Username.jsx
+++ fast-react-pizza/src/features/users/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold hidden">Ronny</div>;
+  return <div className="text-sm font-semibold hidden md:block">Ronny</div>;
 }
 
 export default Username;
