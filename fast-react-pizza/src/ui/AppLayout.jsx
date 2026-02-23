faeabfa453ffc4900a506a4f4116babb85e5fad3
Rachit Jain <rachitjain9596@gmail.com>
2026-02-23 10:30:00 +0530

    fast-react-pizza tailwind loader styling

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -8,8 +8,9 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] h-screen ">
-      {isLoading && <Loading />}
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {/* {isLoading && <Loading />} */}
+      {true && <Loading />}
 
       <Header />
 
