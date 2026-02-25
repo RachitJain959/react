a0f4dc7faaeaf027be6315955e920c15b04c65da
Rachit Jain <rachitjain9596@gmail.com>
2026-02-25 10:30:00 +0530

    fast-react-pizza tailwind cart

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 text-xs md:px-5 md:py-2.5",
+    secondary:
+      "inline-block rounded-full text-stone-400 px-3.5 py-2.5 border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-200 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed",
   };
 
   if (to) {
